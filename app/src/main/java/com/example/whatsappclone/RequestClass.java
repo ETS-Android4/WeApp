00d128a64873ffc65546364b8085f5189492d09b
blank0826 <aditya26052002@gmail.com>
2021-11-15 14:47:47 +0530

    WeApp

---
+++ app/src/main/java/com/example/whatsappclone/RequestClass.java
@@ -0,0 +1,21 @@
+package com.example.whatsappclone;
+
+public class RequestClass {
+String type;
+
+    public RequestClass() {
+        return;
+    }
+
+    public RequestClass(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ app/src/main/java/com/example/whatsappclone/sendNotificationCode/Response.java
@@ -0,0 +1,7 @@
+package com.example.whatsappclone.sendNotificationCode;
+
+public class Response {
+
+    public int success;
+
+}
